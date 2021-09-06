8cd0dfc89852b3118bed6078e5dcbf75899874b6
Niklas Sivaro <niklas.sivaro@columbiaroad.com>
2021-09-06 21:55:46 +0300

    3.8

--- 3/phonebook-backend/index.js
+++ 3/phonebook-backend/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('payload', function getPayload(req) {
+  return req.body && JSON.stringify(req.body)
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :payload'))
 
 let persons = [
   {
