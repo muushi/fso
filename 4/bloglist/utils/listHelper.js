b999daa883dd2542bf0d138745bff6cb1a93df4c
Niklas Sivaro <niklas.sivaro@columbiaroad.com>
2021-12-22 11:49:42 +0200

    4.5

--- 4/bloglist/utils/listHelper.js
+++ 4/bloglist/utils/listHelper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.map(blog => blog.likes).reduce((acc, cur) => acc + cur, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((prev, cur) => (prev.likes > cur.likes) ? prev : cur, {})
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
