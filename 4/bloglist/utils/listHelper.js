74d6db45a7e96e9835f9429c3f8dd3483e388105
Niklas Sivaro <niklas.sivaro@columbiaroad.com>
2021-12-22 12:31:01 +0200

    4.6

--- 4/bloglist/utils/listHelper.js
+++ 4/bloglist/utils/listHelper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -10,8 +12,21 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce((prev, cur) => (prev.likes > cur.likes) ? prev : cur, {})
 }
 
+const mostBlogs = (blogs) => {
+  const grouped = _.groupBy(blogs, 'author')
+  Object.keys(grouped).forEach((key) => {
+    grouped[key] = grouped[key].length
+  })
+  const resArr = Object.entries(grouped)[Object.values(grouped).indexOf(Math.max(...Object.values(grouped)))]
+  return {
+    author: resArr[0],
+    blogs: resArr[1]
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
